faa0df050b0f985b76545976202e9e62fb47c9d8
sanya6456 <66361881+sanya6456@users.noreply.github.com>
2020-06-11 09:04:34 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+var mySwiper = new Swiper('.pagination', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+});
+
+$(document).ready(function(){
+    alert('jquery ready');
+});
