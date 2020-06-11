267b0cf709ffc3d8c25558423b2f5a2a67960e86
sanya6456 <66361881+sanya6456@users.noreply.github.com>
2020-06-11 14:01:23 +0200

    update01

--- main.js
+++ main.js
@@ -1,10 +1,38 @@
-var mySwiper = new Swiper('.pagination', {
+//active button
+$(document).ready(function(){
+  $('.btn').click(function(){
+    $(this).addClass('active');
+    $(this).siblings().removeClass('active');
+  });
+});
+//_________
+
+//active nav
+$(document).ready(function(){
+  $('.first-nav').click(function(){
+    $('.first-nav').removeClass('activenav');
+    $(this).addClass('activenav');
+  });
+});
+
+$(document).ready(function(){
+  $('.second-nav').click(function(){
+    $('.second-nav').removeClass('activenav');
+    $(this).addClass('activenav');
+  });
+});
+//__________
+
+//swiper
+$(document).ready(function () {
+  //initialize swiper when document ready
+  var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    loop: true,
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
+  })
 });
-
-$(document).ready(function(){
-    alert('jquery ready');
-});
+//________
